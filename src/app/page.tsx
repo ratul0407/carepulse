4f67bdccbb738e58d9701a63cfcc1b454a936b17
ratul0407 <rajaulislamratul12@gmail.com>
2025-03-09 16:28:02 +0600

    started working on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import Image from "next/image";
+import logoFull from "../../public/assets/icons/logo-full.svg";
+import { PatientForm } from "@/components/forms/PatientForm";
+import Link from "next/link";
 export default function Home() {
-  return <h1 className="text-3xl">Hello world</h1>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+          />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              &copy; 2025 careplus
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="hero image"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
 }
---
+++ src/components/forms/PatientForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PatientForm = () => {
+  return <div>PatientForm</div>;
+};
